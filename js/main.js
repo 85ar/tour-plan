bde34b2557d67e12c37b92668e9ef8cd65300deb
85ar <85ar@rambler.ru>
2021-03-21 21:37:40 +0500

    create: modal windows

--- js/main.js
+++ js/main.js
@@ -1,43 +1,60 @@
-var hotelSlider = new Swiper(".hotel-slider", {
-  loop: true,
+$(document).ready(function () {
+  var hotelSlider = new Swiper(".hotel-slider", {
+    loop: true,
 
-  keyboard: {
-    enabled: true,
-  },
+    keyboard: {
+      enabled: true,
+    },
 
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-  effect: "fade",
-});
-var reviewsSlider = new Swiper(".reviews-slider", {
-  loop: true,
-
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-});
-
-
-ymaps.ready(init);
-
-function init() {
-  var myMap = new ymaps.Map("map", {
-    center: [7.89074, 98.294774],
-    zoom: 16,
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+    effect: "fade",
   });
-var myPlacemark = new ymaps.Placemark([7.89074, 98.294774], {
-  hintContent: "Grand Hilton Hotel",
-});
-myMap.geoObjects.add(myPlacemark);
-}
+  var reviewsSlider = new Swiper(".reviews-slider", {
+    loop: true,
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log("Клик по кнопке Меню");
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom--visible");
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+  });
+
+  ymaps.ready(init);
+
+  function init() {
+    var myMap = new ymaps.Map("map", {
+      center: [7.89074, 98.294774],
+      zoom: 16,
+    });
+    var myPlacemark = new ymaps.Placemark([7.89074, 98.294774], {
+      hintContent: "Grand Hilton Hotel",
+    });
+    myMap.geoObjects.add(myPlacemark);
+  }
+
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on('click', openModal)
+  closeModalButton.on('click', closeModal)
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+   function closeModal(event) {
+     event.preventDefault();
+     var modalOverlay = $(".modal__overlay");
+     var modalDialog = $(".modal__dialog");
+     modalOverlay.removeClass("modal__overlay--visible");
+     modalDialog.removeClass("modal__dialog--visible");
+   }
 });
